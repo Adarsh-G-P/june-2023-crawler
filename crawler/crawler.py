7e64722e22c618f1e60fab228e7a64737b0bc84a
Adarsh-G-P <adarshgp96@gmail.com>
2023-06-05 09:59:56 +0530

    lyrics test added

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,3 +8,26 @@ def get_artists(data):
         a = i.find("a") # Get the anchor inside the td
         ret.append((a.text.strip(), a["href"])) # Extract the name and target from anchor
     return ret
+
+def get_songs(data):
+    soup = BeautifulSoup(data, features="html.parser") # Create soup
+    songs = soup.find_all("table", {"class": "tracklist"})
+    ret = []
+    for i in songs: # 
+        a = i.find("a") # 
+        ret.append((a.text.strip(), a["href"])) # Extract the name and target from anchor
+    return ret
+
+def get_lyrics(data):
+    soup = BeautifulSoup(data, features="html.parser") # Create soup
+    lyrics = soup.find("p", {"id" :"songLyricsDiv"})
+    ret = []
+    # for i in lyrics:
+    #     a = i.find("a")
+    #     ret.append((a.text, a["href"]))
+    if lyrics:
+        ret = lyrics.text
+   
+    return ret
+
+
--- crawler/tests/test_crawler.py
+++ crawler/tests/test_crawler.py
@@ -25,4 +25,41 @@ def test_get_popular_artists():
     assert name100 == "Shakira"
     assert link100 == "https://www.songlyrics.com/shakira-lyrics/"
 
+def test_get_list_songs():
+    data_file = os.path.join(os.path.dirname(__file__), "data", "list_songs.html")
+    with open(data_file) as f:
+        data = f.read()
+    songs = crawler.get_songs(data)
+    song1, link1 = songs[0] 
+    # song2, link2 = songs[1]
+    # songs482, link482 = songs[481]
+    # songs483, link483 = songs[482]
+
+    # assert len(songs) == 483      
     
+    assert song1 == "Oceans (Where Feet May Fail)"
+    assert link1 == "https://www.songlyrics.com/hillsong/oceans-where-feet-may-fail-lyrics/"
+
+    # assert song2 == "This Is Our God"
+    # assert link2 == "https://www.songlyrics.com/hillsong/this-is-our-god-lyrics/"
+     
+    # assert songs482 == "In Your Hands"
+    # assert link482 == "https://www.songlyrics.com/hillsong/in-your-hands-lyrics/"
+    
+    # assert songs483 == "God Is In The House(Live)"
+    # assert link483 == "https://www.songlyrics.com/hillsong/god-is-in-the-house-live-lyrics/"
+    
+
+
+def test_get_lyrics():
+    data_file = os.path.join(os.path.dirname(__file__), "data", "lyrics.html")
+    with open(data_file) as f:
+        data = f.read()
+    lyrics = crawler.get_lyrics(data)
+    assert lyrics.startswith("You call me out upon the waters")
+    print(lyrics)
+
+
+    # lyrics_start_with, lyrics_end_with = songs[0]
+    # assert lyrics_start_with.startwith() == "You call me out upon the waters"
+    # assert lyrics_end_with == "You call me out upon the waters"
