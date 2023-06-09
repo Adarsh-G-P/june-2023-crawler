c024ef61b6b971be33359180fe39edf83799742e
Adarsh-G-P <adarshgp96@gmail.com>
2023-06-09 10:34:51 +0530

    updated requirements for display

---
+++ crawler/cli.py
@@ -0,0 +1,76 @@
+import argparse
+
+import crawler
+import requests
+def parse():
+    parser = argparse.ArgumentParser(
+        prog = "lyrics",
+        description = "Offline song lyrics browser")
+    
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.add_parser("listartists", help = "List of artists in the system")
+    subparsers.add_parser("initdb", help = "Initialise the database")
+    subparsers.add_parser("song", help = "song crawl")
+    crawl_parser = subparsers.add_parser("crawl", help = "Crawl lyrics")
+    song_parser = subparsers.add_parser("song", help = "Song lyrics")
+
+
+    crawl_parser.add_argument("--nartists", help="Number of artists to crawl (Default : %(default)s)", 
+                              type=int, 
+                              default=8)
+
+    crawl_parser.add_argument("--ntracks", help="Number of tracks to crawl per artist (Default : %(default)s)",
+                              type=int,
+                              default=5)
+    song_parser.add_argument("--artist",help = "Name of artist to crawl (Default:%(default)s)",
+                            type = str,
+                            # default =
+                            )
+    song_parser.add_argument("--ntracks",help = "Number of tracks to crawl as per artist(Default:%(default)s)",
+                            type = int,
+                            default = 5
+                            )
+
+    args = parser.parse_args()
+    return args
+
+def handle_listartists(args):
+    artists = crawler.get_artists()
+    for idx, name in enumerate(artists, start=1):
+        print (f"{idx}. {name}")
+
+def handle_initdb(args):
+    crawler.initdb()
+
+def handle_crawl(args):
+    print (args)
+    crawler.crawl("https://www.songlyrics.com/top-artists-lyrics.html", 
+                    args.nartists, 
+                    args.ntracks)
+
+def handle_song(args):
+    artist = args.artist
+    print(artist)
+    url = "https://www.songlyrics.com/" + artist.lower() + "-lyrics/"
+    print(url)
+    song = crawler.get_song(artist)
+
+    if song is not None:
+        for i in song:
+            print(i)
+
+    else:
+        print("Sorry, no songs found for artist", artist)
+
+
+def main():
+    commands = {"listartists" : handle_listartists,
+                "initdb"  : handle_initdb ,
+                "crawl" : handle_crawl,
+                "song" : handle_song}
+
+    args = parse()
+    commands[args.command](args)
+
+if __name__ == "__main__":
+    main()
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -6,45 +6,48 @@ from bs4 import BeautifulSoup
 
 db = psycopg2.connect("dbname=students")
 
-def get_artists(data, count=10):
-    """
-    Inputs :
-      1. HTML string which contains a list of artists. 
-      2. Count is the maximum number of artists that will be returned.
-
-    Outputs:
-      1. A list each element of which is of the form
-         ("artist name", link to tracks of artist)
-
-    Description of what this does:
-      Parses the input HTML, find the list of artists. Creates a list as mentioned in outputs and returns the list
+# DB functions
+def initdb():
+    cur = db.cursor()
+    with open("init.sql") as f:
+        cur.execute(f.read())
+    db.commit()
+    cur.close()
     
-    """
+def get_artists():
+    cur = db.cursor()
+    cur.execute("SELECT name from artists ORDER BY name")
+    artists = cur.fetchall()
+    ret = []
+    for i in artists:
+        ret.append(i[0])
+    cur.close()
+    return ret
 
+# def get_song():
+#     cur = db.cursor()
+#     cur.execute
+
+# Crawler functions
+def crawl_artists(data, count=10):
+    
     soup = BeautifulSoup(data, features="html.parser") # Create soup
     artists = soup.find_all("td", {"class": "td-last"}) # Search for all artist td nodes
     ret = []
     for i in artists: # For each td node
         a = i.find("a") # Get the anchor inside the td
         ret.append((a.text.strip(), a["href"])) # Extract the name and target from anchor
-        if count == 0:
-            break
-        count -= 1
+        if count is not None:
+            if count.isnumeric():
+                count = int(count) - 1
+
+                if count == 0:
+                    break
+
     return ret
 
-def get_tracks_of_artist(data, count=5):
-    """
-    Inputs : 
-      HTML string which contains all tracks of a single artist
-
-    Outputs :
-      A list each element of which is of the form
-      ("track name", "lyrics of the song")
-
-    Description : 
-      Parses the input HTML to find all the tracks of the artist. Creates a list like mentioned in the output
-    
-    """
+def crawl_tracks_of_artist(data, count=5):
+   
     soup = BeautifulSoup(data, features="html.parser")
     tracks = soup.find("table", {"class" : "tracklist"})
     ret = []
@@ -52,9 +55,11 @@ def get_tracks_of_artist(data, count=5):
         lyrics_page = requests.get(track['href']).text
         lyrics = extract_lyrics(lyrics_page)
         ret.append([track.text.strip(), lyrics])
-        if count == 0:
-            break
-        count -=1
+        if count is not None:
+            count -=1
+            if count == 0:
+                break
+
     return ret
     
 def extract_lyrics(data):
@@ -77,30 +82,13 @@ def save_track_to_db(artist, track, lyrics, db=db):
         cur.execute("INSERT INTO artists (name) VALUES(%s)", (artist,));
         cur.execute("SELECT id from artists where name = %s", (artist,))
         artist_id = cur.fetchone()[0]
-    print("Id is ", artist_id)
-
-    cur.execute("INSERT INTO tracks (artists_id,name,lyrics) values (%s,%s,%s)",(artist_id,track,lyrics,))
-    # cur.execute ("select id from tracks where artists_id=%s",(artist_id,track))
+    cur.execute("INSERT INTO tracks (artist_id, name, lyrics) VALUES (%s, %s, %s)", (artist_id, track, lyrics));
     db.commit()
     cur.close()
 
 
 def save_track(artist, track, lyrics, base="lyrics_crawl"):
-    """
-    Inputs : 
-       1. artist - String containing the name of the artist
-       2. track - String containing name of the track of the artist
-       3. Lyrics - String containing lyrics of the above track
-       4. base - Base directory to store the artist lyrics
-
-    Outputs: 
-       None
-
-    Description: 
-       Creates files of the forms
-    {artist_name}/{track_name}.txt and stores the lyrics inside them.
-
-    """
+   
 
     artist = artist.replace("/","_").replace(" ","_").lower()
     track = track.replace("/","_").replace(" ","_").lower()
@@ -115,18 +103,26 @@ def save_track(artist, track, lyrics, base="lyrics_crawl"):
         f.write(lyrics)
     
 
-def crawl(start_url):
+def crawl(start_url, nartists, ntracks):
     data = requests.get(start_url).text
-    artists = get_artists(data)
+    artists = crawl_artists(data, nartists)
     for artist_name, artist_link in artists:
         print (f"{artist_name} : ", end="", flush = True)
         tracks_page = requests.get(artist_link).text
-        tracks = get_tracks_of_artist(tracks_page)
+        tracks = crawl_tracks_of_artist(tracks_page, ntracks)
         for track_name, lyrics in tracks:
             save_track(artist_name, track_name, lyrics)
             save_track_to_db(artist_name, track_name, lyrics)
             print (".", end="", flush=True)
         print()
 
-if __name__ == "__main__":
-    crawl("https://www.songlyrics.com/top-artists-lyrics.html")
+def get_song(artist_name):
+    cur = db.cursor()
+    cur.execute("SELECT tracks.name FROM tracks JOIN artists ON tracks.artist_id = artists.id WHERE artists.name = %s", (artist_name,))
+    tracks = cur.fetchall()
+    result = []
+    cur.close()   
+    for track in tracks:
+        result.append(track[0])   
+
+    return result
